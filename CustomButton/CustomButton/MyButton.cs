abdfbf51f5f2845ff663c780a8c6e2db6eccb0a9
André <AndreR@ppedv.de>
2024-03-20 16:34:14 +0100

    Custom WinForms Button

---
+++ CustomButton/CustomButton/MyButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomButton
+{
+    internal class MyButton : Button
+    {
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            pevent.Graphics.FillRectangle(Brushes.Pink, ClientRectangle);
+            pevent.Graphics.FillEllipse(Brushes.Blue, ClientRectangle);
+
+        }
+    }
+}
