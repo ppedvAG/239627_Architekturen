eb6fa157905de0bd23ce3450a166799f59982327
André <AndreR@ppedv.de>
2024-03-20 11:51:29 +0100

    Builder

---
+++ HalloBuilder/HalloBuilder/Program.cs
@@ -0,0 +1,13 @@
+﻿using HalloBuilder;
+
+Console.WriteLine("Hello, World!");
+
+var schrank = new Schrank.Builder()
+                         .SetAnzBöden(4)
+                         .SetAnzTüren(6)
+                         .Create();
+
+var schrank2 = new Schrank.Builder()
+                         .SetAnzTüren(6)
+                         .SetAnzBöden(12)
+                         .Create();
