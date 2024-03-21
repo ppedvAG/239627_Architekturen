7a882c0b160272c4a2770cd64a3ba6037c3f3fe1
André <AndreR@ppedv.de>
2024-03-21 14:41:36 +0100

    Schönes Consolen UI

---
+++ ppedv.CubesPizza/ppedv.CubesPizza.UI.DevConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using ppedv.CubesPizza.Core;
+using ppedv.CubesPizza.Model;
+using ppedv.CubesPizza.Model.Contracts;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("🍕🍕🍕                  🍕🍕🍕");
+Console.WriteLine("🍕🍕🍕 Cubes Pizza v0.1 🍕🍕🍕");
+Console.WriteLine("🍕🍕🍕                  🍕🍕🍕");
+
+string conString = "Server=(localdb)\\mssqllocaldb;Database=CubesPizza_Tests;Trusted_Connection=true;";
+
+IRepository repo = new ppedv.CubesPizza.Data.EfCore.PizzaContextRepositoryAdapter(conString);
+FoodService fs = new FoodService(repo);
+
+Console.WriteLine("Speisekarte");
+foreach (var p in fs.GetSpeisekarte())
+{
+    Console.WriteLine($"{p.Name} {p.Price:c} {(p.IsVegetarian ? "🥦" : "🍖")}");
+}
+
+
+Console.WriteLine("Bestellungen");
+foreach (var o in repo.GetAll<Order>())
+{
+    Console.WriteLine($"{o.OrderDate:g} {o.CustomerName}");
+}
