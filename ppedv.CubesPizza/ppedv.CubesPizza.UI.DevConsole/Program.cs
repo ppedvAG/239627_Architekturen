b8817dcafdce35150e934c076d8926da5e7320f5
André <AndreR@ppedv.de>
2024-03-22 15:58:17 +0100

    Order per Console

--- ppedv.CubesPizza/ppedv.CubesPizza.UI.DevConsole/Program.cs
+++ ppedv.CubesPizza/ppedv.CubesPizza.UI.DevConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac;
 using Autofac.Core;
 using ppedv.CubesPizza.Core;
+using ppedv.CubesPizza.Devices.BinFord;
 using ppedv.CubesPizza.Model;
 using ppedv.CubesPizza.Model.Contracts;
 using System.Reflection;
@@ -27,15 +28,24 @@ string conString = "Server=(localdb)\\mssqllocaldb;Database=CubesPizza_Tests;Tru
 
 //DI per AutoFac
 var builder = new ContainerBuilder();
-builder.RegisterType<FoodService>();
-builder.Register(x => new ppedv.CubesPizza.Data.EfCore.PizzaContextRepositoryAdapter(conString)).As<IRepository>();
+builder.RegisterType<FoodService>().As<IFoodService>();
+builder.RegisterType<OrderService>().As<IOrderService>();
+builder.RegisterType<BinFordOvenControl>().As<IOvenControl>();
+builder.Register(x => new ppedv.CubesPizza.Data.EfCore.PizzaContextRepositoryAdapter(conString)).As<IRepository>().SingleInstance();
 var container = builder.Build();
 
 var repo = container.Resolve<IRepository>();
-var fs = container.Resolve<FoodService>();
+var fs = container.Resolve<IFoodService>();
+var speisen = fs.GetSpeisekarte();
+var os = container.Resolve<IOrderService>();
+
+var testOrder = new Order();
+testOrder.BillingAddress = new Address() { Line1 = "Fred Feuerstein", Street = "Steinstr. ", City = "?" };
+testOrder.Items.Add(new OrderItem() { Amount = 2, ItemPrice = 12.40m, Pizza = speisen.ElementAt(3) });
+os.PlaceOrder(testOrder);
 
 Console.WriteLine("Speisekarte");
-foreach (var p in fs.GetSpeisekarte())
+foreach (var p in speisen)
 {
     Console.WriteLine($"{p.Name} {p.Price:c} {(p.IsVegetarian ? "🥦" : "🍖")}");
 }
