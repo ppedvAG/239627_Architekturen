75853926802afc42b219fa3dae92cd136d9cf79b
André <AndreR@ppedv.de>
2024-03-22 12:00:39 +0100

    Google Books Client

---
+++ BooksClient/BooksClient/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace BooksClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(66, 53);
+            textBox1.Margin = new Padding(4, 4, 4, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(505, 39);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "WPF";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(123, 137);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(360, 225);
+            dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(630, 52);
+            button1.Name = "button1";
+            button1.Size = new Size(169, 55);
+            button1.TabIndex = 2;
+            button1.Text = "Such";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1258, 475);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox1);
+            Font = new Font("Segoe UI", 12F);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private DataGridView dataGridView1;
+        private Button button1;
+    }
+}
---
+++ BooksClient/BooksClient/Form1.cs
@@ -0,0 +1,23 @@
+using System.Text.Json;
+
+namespace BooksClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var url = $"https://www.googleapis.com/books/v1/volumes?q={textBox1.Text}";
+            var http = new HttpClient();
+            var json = await http.GetStringAsync(url);
+
+            BooksResult br = JsonSerializer.Deserialize<BooksResult>(json);
+
+            dataGridView1.DataSource = br.items.Select(x => x.volumeInfo).ToList();
+        }
+    }
+}
